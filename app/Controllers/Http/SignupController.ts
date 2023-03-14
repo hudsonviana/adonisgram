c7d3cbb42cadff28d95c05c81a55f3aaa8dca3eb
hudsonviana <hudson1206@gmail.com>
2023-03-14 00:30:01 -0300

    3 AdonisJS crash course (Build Instagram Clone) - Validation Rules

--- app/Controllers/Http/SignupController.ts
+++ app/Controllers/Http/SignupController.ts
@@ -1,7 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class SignupController {
   public async index({ request }: HttpContextContract) {
+    const req = await request.validate({
+      schema: schema.create({
+        name: schema.string(),
+        email: schema.string({}, [rules.email()]),
+        password: schema.string({}, [rules.confirmed()]),
+      }),
+      messages: {
+        required: 'O campo {{ field }} é obrigatório.',
+        'email.unique': 'Email não disponível.',
+      },
+    })
+    console.log(req)
     return request.all()
   }
 }
